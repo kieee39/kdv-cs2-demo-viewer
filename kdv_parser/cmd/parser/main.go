81ebc91b92fef1ab445dce4d263c922167eadf91
kieee39 <kieee39@users.noreply.github.com>
2026-02-24 23:19:32 +0900

    chore(kdv_parser): output the version of demoinfocs-golang before parsing

--- kdv_parser/cmd/parser/main.go
+++ kdv_parser/cmd/parser/main.go
@@ -1,12 +1,19 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"runtime/debug"
+
 	"kdv_parser/internal/config"
 	"kdv_parser/internal/pipeline"
 )
 
+const demoInfocsModule = "github.com/markus-wa/demoinfocs-golang/v5"
+
 func main() {
+	printDemoInfocsVersion()
+
 	cfg, err := config.FromFlags()
 	if err != nil {
 		log.Fatalf("failed to parse flags: %v", err)
@@ -14,4 +21,26 @@ func main() {
 	if err := pipeline.Run(cfg); err != nil {
 		log.Fatalf("failed to run pipeline: %v", err)
 	}
+}
+
+func printDemoInfocsVersion() {
+	info, ok := debug.ReadBuildInfo()
+	if !ok {
+		fmt.Printf("[startup] demoinfocs-golang: unknown (build info unavailable)\n")
+		return
+	}
+
+	for _, dep := range info.Deps {
+		if dep.Path != demoInfocsModule {
+			continue
+		}
+		if dep.Replace != nil {
+			fmt.Printf("[startup] demoinfocs-golang: %s (replaced by %s %s)\n", dep.Version, dep.Replace.Path, dep.Replace.Version)
+			return
+		}
+		fmt.Printf("[startup] demoinfocs-golang: %s\n", dep.Version)
+		return
+	}
+
+	fmt.Printf("[startup] demoinfocs-golang: unknown (module not found in build info)\n")
 }
